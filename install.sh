10da53fc7fad5ac6f310e20c45c2eaf57a27c022
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-12-24 02:40:51 +0100

    Use basename

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 install () {
-    TARGET=$1 DIR=$2
-    sudo rm -rf $DIR$TARGET
-    sudo ln -nrs dotfiles/$TARGET $DIR$TARGET &&
+    TARGET=$1 DESTINATION=$2$(basename $TARGET)
+    sudo rm -rf $DESTINATION
+    sudo ln -rs dotfiles/$TARGET $DESTINATION &&
     echo Installed: $TARGET
 }
 
-install .bash_profile           ~/
-install .bashrc                 ~/
-install .xinitrc                ~/
-install git                     ~/.config/
-install nvim                    ~/.config/
-install pacman.conf             /etc/
+install .bash_profile       ~/
+install .bashrc             ~/
+install .xinitrc            ~/
+install git                 ~/.config/
+install nvim                ~/.config/
+install pacman/pacman.conf  /etc/
+install pacman/hooks/       /etc/pacman.d/
