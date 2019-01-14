2afb981ffb2ea785402e667bb673df643697f8fa
astier <aleksandrs.stier@uni-bielefeld.de>
2019-01-15 00:38:23 +0100

    Add vscode-keybindings

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ install () {
     echo Installed: "$TARGET"
 }
 
-install .bash_profile           ~/
-install .bashrc                 ~/
-install .xinitrc                ~/
-install git                     ~/.config/
-install htop                    ~/.config/
-install vscode/settings.json    ~/.config/Code/User/
-install pacman/pacman.conf      /etc/
-install pacman/hooks/           /etc/pacman.d/
+install .bash_profile               ~/
+install .bashrc                     ~/
+install .xinitrc                    ~/
+install git                         ~/.config/
+install htop                        ~/.config/
+install vscode/keybindings.json     ~/.config/Code/User/
+install vscode/settings.json        ~/.config/Code/User/
+install pacman/pacman.conf          /etc/
+install pacman/hooks/               /etc/pacman.d/
