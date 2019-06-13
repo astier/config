cde60be276ecc2d31c7e2dca578925464090ea21
aleks <aleks.stier@icloud.com>
2019-06-13 12:08:36 +0200

    Replace dwm with cinnamon

--- install.sh
+++ install.sh
@@ -14,17 +14,6 @@ install .xinitrc ~/
 install bash/.bash_profile ~/
 install bash/.bashrc ~/
 install git ~/.config/
-install gtk-3.0 ~/.config/
 install nvim ~/.config/
 install pacman/hooks/ /etc/pacman.d/
 install pacman/pacman.conf /etc/
-
-sudo rm /etc/systemd/journald.conf
-sudo ln ~/Projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd/journald.conf &&
-	echo Installed: journald
-sudo rm /etc/systemd/logind.conf
-sudo ln ~/Projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd/logind.conf &&
-	echo Installed: logind
-sudo rm /etc/systemd/network/*
-sudo ln ~/Projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network/ &&
-	echo Installed: networkd
