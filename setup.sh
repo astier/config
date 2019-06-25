99f057327bfe9e22c7939705adb9b8bd74934285
aleks <aleks.stier@icloud.com>
2019-06-25 21:30:11 +0200

    Revert "Switch from suckless to cinnamon"
    This reverts commit 4fdc6efe4c7384051df32e90dcc3eeead8012947.

--- setup.sh
+++ setup.sh
@@ -14,7 +14,9 @@ install .xinitrc ~/
 install bash/.bash_profile ~/
 install bash/.bashrc ~/
 
+install dunst ~/.config/
 install git ~/.config/
+install gtk-3.0 ~/.config/
 install nvim ~/.config/
 
 install pacman/hooks/ /etc/pacman.d/
