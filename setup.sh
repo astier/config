885ff948950dcf6da352cf2e399358c7d03797a4
aleks <aleks.stier@icloud.com>
2019-07-05 18:45:53 +0200

    Add bootloader and mkinitcpio.conf

--- setup.sh
+++ setup.sh
@@ -7,28 +7,33 @@ install() {
 		echo Installed: "$TARGET"
 }
 
-install .inputrc ~/
-install .tmux.conf ~/
-install .xinitrc ~/
-install bash/.bash_profile ~/
-install bash/.bashrc ~/
+install .inputrc ~
+install .tmux.conf ~
+install .xinitrc ~
+install bash/.bash_profile ~
+install bash/.bashrc ~
 
-install git ~/.config/
-install gtk-3.0 ~/.config/
-install nvim ~/.config/
+install git ~/.config
+install gtk-3.0 ~/.config
+install nvim ~/.config
 
-install pacman/hooks/ /etc/pacman.d/
-install pacman/pacman.conf /etc/
+install mkinitcpio.conf /etc
+install pacman/hooks/ /etc/pacman.d
+install pacman/pacman.conf /etc
+
+sudo cp -f dotfiles/loader/loader.conf /boot &&
+	sudo cp -f dotfiles/loader/entries/arch.conf /boot/loader/entries &&
+	echo Installed: boot
 
 [ ! -d /etc/systemd/system/getty@tty1.service.d ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d/ &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
 	echo Installed: getty@tty1.service.d
 
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd/ &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd &&
 	echo Installed: journald
 
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd/ &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd &&
 	echo Installed: logind
 
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network/ &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network &&
 	echo Installed: networkd
