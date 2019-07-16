13372e5dc646fb9c461ea8591d00a9618ac1c3bb
aleks <aleks.stier@icloud.com>
2019-07-16 16:40:35 +0200

    Revert "Switch to mint"
    This reverts commit 236e87ea2f8670a3ae50f6dffe67f9ac74bf3b2f.

--- setup.sh
+++ setup.sh
@@ -10,12 +10,37 @@ install() {
 cd dotfiles || exit
 
 install .aliases ~
+install .bash_profile ~
+install .bashrc ~
+install .inputrc ~
 install .profile ~
 install .tmux.conf ~
-install bash/.bash_profile ~
-install bash/.bashrc ~
-install bash/.inputrc ~
+install .xinitrc ~
+
 install git ~/.config
+install gtk-3.0 ~/.config
 install nvim ~/.config
-install xmodmap/.Xmodmap ~
-install xmodmap/xmodmap.desktop ~/.config/autostart
+
+install mkinitcpio.conf /etc
+install pacman/hooks /etc/pacman.d
+install pacman/pacman.conf /etc
+
+sudo iptables-restore < iptables.rules &&
+	echo Installed: iptables
+
+sudo cp -f loader/loader.conf /boot &&
+	sudo cp -f loader/entries/arch.conf /boot/loader/entries &&
+	echo Installed: boot
+
+[ ! -d /etc/systemd/system/getty@tty1.service.d ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
+	echo Installed: getty@tty1.service.d
+
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd &&
+	echo Installed: journald
+
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd &&
+	echo Installed: logind
+
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network &&
+	echo Installed: networkd
