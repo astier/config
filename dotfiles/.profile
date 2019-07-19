626ea54483b87457a6c5b3eb305f88438e9ebf04
aleks <aleks.stier@icloud.com>
2019-07-19 15:12:49 +0200

    Switch to mint

--- dotfiles/.profile
+++ dotfiles/.profile
@@ -1,11 +1,6 @@
 export EDITOR=nvim
 export VISUAL=nvim
-
 export BROWSER=firefox
 export MANPAGER="nvim -c 'set ft=man' -"
-export _JAVA_AWT_WM_NONREPARENTING=1
-
 export FZF_DEFAULT_OPTS="--ansi --cycle -m --reverse --tabstop=4 --color=bg+:-1,fg+:-1,border:#000000"
 export FZF_DEFAULT_COMMAND="find . -type f ! -path '*/\.git/*' ! -path '*/tex/*' ! -iname 'tags' ! -iname '*\.jpg' ! -iname '*\.jpeg' ! -iname '*\.png' ! -iname '*\.pdf' ! -iname '*\.gif' ! -iname '*\.css' ! -iname '*\.html' ! -iname '*\.js' ! -iname '*\.htm' ! -iname '*\.docx' ! -iname '*\.doc' ! -iname '*\.odt' ! -iname '*\.zip' ! -iname '*\.tar.gz' | sed 's/^.\///'"
-
-[ "$(tty)" = "/dev/tty1" ] && [ "$(whoami)" != "root" ] && startx
--- setup.sh
+++ setup.sh
@@ -13,30 +13,10 @@ install .Xmodmap ~
 install .aliases ~
 install .profile ~
 install .tmux.conf ~
-install .xinitrc ~
 install bash/.bash_profile ~
 install bash/.bashrc ~
 install bash/.inputrc ~
 
 install git ~/.config
-install gtk-3.0 ~/.config
 install nvim ~/.config
-
-install mkinitcpio.conf /etc
-install pacman.conf /etc
-
-sudo iptables-restore < iptables.rules &&
-	echo Installed: iptables
-
-[ ! -d /etc/systemd/system/getty@tty1.service.d ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
-	echo Installed: getty@tty1.service.d
-
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd &&
-	echo Installed: journald
-
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd &&
-	echo Installed: logind
-
-sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network &&
-	echo Installed: networkd
+install xmodmap.desktop ~/.config/autostart
