b4f000142f8eb4cca4604b0b0c471d8a58d557f0
aleks <aleks.stier@icloud.com>
2019-08-20 13:15:13 +0200

    Simplify emptybar and fix for hlwm-7.0

--- dotfiles/herbstluftwm/emptybar.sh
+++ dotfiles/herbstluftwm/emptybar.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
 if ! pgrep -x dzen2 >/dev/null; then
-    hc() { "${herbstclient_command[@]:-herbstclient}" "$@" ;}
-    monitor=${1:-0}
-    geometry=( $(hc monitor_rect "$monitor") )
-    if [ -z "$geometry" ] ;then
-        echo Invalid monitor $monitor
-        exit 1
-    fi
-    x=${geometry[0]}
-    y=${geometry[1]}
-    panel_width=${geometry[2]}
-    panel_height=16
-    dzen2 -w ${geometry[2]} -x ${geometry[0]} -y ${geometry[3]} -h 7 -bg '#000000'
+    geometry=( $(herbstclient monitor_rect 0) )
+    herbstclient --idle 2> /dev/null | dzen2 -y "${geometry[3]}" -h 7 -bg '#000000' -fg '#000000'
 fi
