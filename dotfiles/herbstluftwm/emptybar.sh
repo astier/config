04f87422afe4ae40200928b040b8ee47aafbddf8
aleks <aleks.stier@icloud.com>
2019-08-24 23:19:06 +0200

    Remove emptybar

--- dotfiles/herbstluftwm/emptybar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if ! pgrep -x dzen2 >/dev/null; then
-    geometry=( $(herbstclient monitor_rect 0) )
-    herbstclient --idle 2> /dev/null | dzen2 -y "${geometry[3]}" -h 7 -bg '#000000' -fg '#000000'
-fi
