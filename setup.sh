de88c54d0fd5a3b4334c6fc767db28ef08b7ddd6
aleks <aleks.stier@icloud.com>
2019-10-09 23:50:59 +0200

    Fix iptables installation

--- setup.sh
+++ setup.sh
@@ -32,9 +32,6 @@ install xkb/xkb.desktop ~/.config/autostart
 install pacman/pacman.conf /etc
 install mkinitcpio.conf /etc
 
-sudo iptables-restore < iptables.rules &&
-    echo Installed: iptables
-
 [ ! -d /etc/systemd/system/getty@tty1.service.d ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
     echo Installed: getty@tty1.service.d
@@ -47,3 +44,6 @@ sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd &&
 
 sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network &&
     echo Installed: networkd
+
+sudo ln -f ~/projects/dotfiles/dotfiles/iptables.rules /etc/iptables/iptables.rules
+    echo Installed: iptables
