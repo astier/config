f121e6ad888da1f1768e148eb2099ef84f756642
aleks <aleks.stier@icloud.com>
2019-10-13 06:29:41 +0200

    Add code
    This reverts commit f58c26b84ed456d35cd2bec16a9e3d53ee7434cd.

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ install nvim ~/.config
 install pacman ~/.config
 install sx ~/.config
 install sxhkd ~/.config
+install code/settings.json ~/.config/Code/User/
 install xkb ~/.config
 install xkb/xkb.desktop ~/.config/autostart
 
