2e3cf37d070b1de46dbc99caf308bfcea27728c8
aleks <aleks.stier@icloud.com>
2019-10-19 21:20:57 +0200

    Move dotfiles out of subfolders

--- setup.sh
+++ setup.sh
@@ -10,31 +10,31 @@ install() {
 [ ! -d ~/.config ] && mkdir ~/.config
 cd dotfiles || exit
 
-install gui/gtk-3.0 ~/.config
-install shell/.bash_profile ~
-install shell/.bashrc ~
-install shell/.profile ~
-install shell/aliases ~/.config
-install shell/inputrc ~/.config
-install system/mkinitcpio.conf /etc
-install system/pacman ~/.config
-install system/pacman/pacman.conf /etc
-install tui/condarc ~/.config
-install tui/git ~/.config
-install tui/nvim ~/.config
-install tui/pylintrc ~/.config
-install tui/tmux.conf ~/.config
-install x/sx ~/.config
-install x/xkb ~/.config
+install .bash_profile ~
+install .bashrc ~
+install .profile ~
+install aliases ~/.config
+install condarc ~/.config
+install git ~/.config
+install gtk-3.0 ~/.config
+install inputrc ~/.config
+install mkinitcpio.conf /etc
+install nvim ~/.config
+install pacman ~/.config
+install pacman/pacman.conf /etc
+install pylintrc ~/.config
+install sx ~/.config
+install tmux.conf ~/.config
+install xkb ~/.config
 
 [ ! -d /etc/systemd/system/getty@tty1.service.d ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d
-sudo ln -f ~/projects/dotfiles/dotfiles/system/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d &&
     echo Installed: getty@tty1.service.d
-sudo ln -f ~/projects/dotfiles/dotfiles/system/systemd/journald.conf /etc/systemd &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/journald.conf /etc/systemd &&
     echo Installed: journald
-sudo ln -f ~/projects/dotfiles/dotfiles/system/systemd/logind.conf /etc/systemd &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/logind.conf /etc/systemd &&
     echo Installed: logind
-sudo ln -f ~/projects/dotfiles/dotfiles/system/systemd/network/* /etc/systemd/network &&
+sudo ln -f ~/projects/dotfiles/dotfiles/systemd/network/* /etc/systemd/network &&
     echo Installed: networkd
-sudo ln -f ~/projects/dotfiles/dotfiles/system/iptables.rules /etc/iptables/iptables.rules
+sudo ln -f ~/projects/dotfiles/dotfiles/iptables.rules /etc/iptables/iptables.rules
     echo Installed: iptables
