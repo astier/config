31f4861aa3694a5eb6d3254aa0d78fe5f99b89f0
aleks <aleks.stier@icloud.com>
2020-03-19 21:00:14 +0100

    [newsboat] add

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ link herbstluftwm ~/.config
 link inputrc ~/.config
 link iptables.rules /etc/iptables
 link mimeapps.list ~/.config/
+link newsboat ~/.config/
 link nvim ~/.config
 link pacman ~/.config
 link pacman/pacman.conf /etc
