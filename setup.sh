265a0a2588253eabab6e549af747e87d96d0c99f
aleks <aleks.stier@icloud.com>
2020-03-24 23:24:02 +0100

    [newsboat] delete

--- setup.sh
+++ setup.sh
@@ -19,7 +19,6 @@ link gtk-3.0 ~/.config
 link inputrc ~/.config
 link iptables.rules /etc/iptables
 link mimeapps.list ~/.config/
-link newsboat ~/.config/
 link nvim ~/.config
 link pacman ~/.config
 link pacman/pacman.conf /etc
