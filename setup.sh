1e1fbea7d0b4479c57b93daa4958dea5367cb2d8
aleks <aleks.stier@icloud.com>
2020-07-09 23:49:30 +0200

    [setup] install certain configs only if in wheel group

--- setup.sh
+++ setup.sh
@@ -1,46 +1,49 @@
 #!/usr/bin/env sh
 
+copy() { sudo cp -fr "$1" "$2" && echo Installed: "$1"; }
+
 link() {
     TARGET=$1 DESTINATION=$2/$(basename "$TARGET")
-    sudo rm -fr "$DESTINATION"
-    sudo ln -rs "$TARGET" "$DESTINATION" &&
-        echo Installed: "$TARGET"
+    rm -fr "$DESTINATION"
+    ln -rs "$TARGET" "$DESTINATION" && echo Installed: "$TARGET"
 }
 
 mkdir -p ~/.config
-cd dotfiles || exit
+cd dotfiles || return
 
 link .bashrc ~
 link .profile ~
 link aliases ~/.config
 link git ~/.config
-link gtk-2.0 ~/.config
-link gtk-3.0 ~/.config
-link herbstluftwm ~/.config
 link inputrc ~/.config
-link iptables.rules /etc/iptables
-link newsboat ~/.config/
 link nvim ~/.config
-link pacman ~/.config
-link pacman/pacman.conf /etc
-link pylintrc ~/.config
 link pythonrc.py ~/.config
-link shellcheckrc ~/.config
-link sx ~/.config
-link sxhkd ~/.config
-link templates ~/.config
-link tint2 ~/.config
 link tmux ~/.config
-link tty-cursor.conf /etc/tmpfiles.d
 link xkb ~/.config
-link zathura ~/.config
 
-copy() { sudo cp -fr "$1" "$2" && echo Installed: "$1"; }
+if groups "$USER" | grep -q wheel; then
 
-copy iwd.conf /etc/iwd/main.conf
-copy systemd/journald.conf /etc/systemd
-copy systemd/logind.conf /etc/systemd
-copy systemd/resolved.conf /etc/systemd
-copy systemd/system.conf /etc/systemd
-copy systemd/system/getty@tty1.service.d /etc/systemd/system
-copy systemd/system/getty@tty2.service.d /etc/systemd/system
+    copy iptables.rules /etc/iptables
+    copy iwd.conf /etc/iwd/main.conf
+    copy pacman/pacman.conf /etc
+    copy systemd/journald.conf /etc/systemd
+    copy systemd/logind.conf /etc/systemd
+    copy systemd/resolved.conf /etc/systemd
+    copy systemd/system.conf /etc/systemd
+    copy systemd/system/getty@tty1.service.d /etc/systemd/system
+    copy systemd/system/getty@tty2.service.d /etc/systemd/system
+    copy tty-cursor.conf /etc/tmpfiles.d
+    link gtk-2.0 ~/.config
+    link gtk-3.0 ~/.config
+    link herbstluftwm ~/.config
+    link newsboat ~/.config/
+    link pacman ~/.config
+    link pylintrc ~/.config
+    link shellcheckrc ~/.config
+    link sx ~/.config
+    link sxhkd ~/.config
+    link templates ~/.config
+    link tint2 ~/.config
+    link zathura ~/.config
+
+fi
