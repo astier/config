a6c566207463d46369f9801a885e32e1a56a2e7d
aleks <aleks.stier@icloud.com>
2020-07-16 04:12:03 +0200

    [alacritty] replace st

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ cd dotfiles || return
 
 link .bashrc ~
 link .profile ~
+link alacritty.yml "$XDG_CONFIG_HOME"
 link aliases "$XDG_CONFIG_HOME"
 link git "$XDG_CONFIG_HOME"
 link gtk-2.0 "$XDG_CONFIG_HOME"
