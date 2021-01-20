3783b34b1594b875172e6be220a096786521e026
aleks <aleks.stier@icloud.com>
2021-01-20 21:52:52 +0100

    [hlwm] delete cycle.sh, swap-windows-binding
    This reverts commit 0559b6eb0ffa3a8fae843543dc73c12c66c40d6d.

--- herbstluftwm/cycle.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-[ "$(herbstclient get_attr tags.focus.tiling.root.0.client_count)" -lt 2 ] && return
-[ "$(herbstclient get_attr tags.focus.tiling.focused_frame.index)" -eq 1 ] && herbstclient focus l
-herbstclient cycle "$1"1
