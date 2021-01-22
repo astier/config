017e7db533a8a5dbd94d8ab9493f689c9d9a252e
aleks <aleks.stier@icloud.com>
2021-01-23 00:11:59 +0100

    [hlwm] make cycle.sh cycle both frame depending on the context
    cycle.sh cycles frames based on the following conditions:
    
    1. Focused frame is cycled if it has more than two windows
    2. Unfocused frame is cycled if it has more than two windows
    
    Otherwise no frame is cycled and the focus remains on the current frame.

--- herbstluftwm/cycle.sh
+++ herbstluftwm/cycle.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-[ "$(herbstclient get_attr tags.focus.tiling.root.0.client_count)" -lt 2 ] && return
-[ "$(herbstclient get_attr tags.focus.tiling.focused_frame.index)" -eq 1 ] && herbstclient focus l
-herbstclient cycle "$1"1
+focused_frame=$(herbstclient get_attr tags.focus.tiling.focused_frame.index)
+unfocused_frame=0; [ "$focused_frame" -eq 0 ] && unfocused_frame=1
+
+if [ "$(herbstclient get_attr tags.focus.tiling.root."$focused_frame".client_count)" -gt 1 ]; then
+    herbstclient cycle "$1"1
+elif [ "$(herbstclient get_attr tags.focus.tiling.root."$unfocused_frame".client_count)" -gt 1 ]; then
+    herbstclient cycle_frame
+    herbstclient cycle "$1"1
+fi
