e135521a291c3d02274e9342757a1c165e86a97a
aleks <aleks.stier@icloud.com>
2021-01-22 23:46:22 +0100

    [hlwm] make cycle.sh only cycle the left frame; remove tiling support

--- herbstluftwm/cycle.sh
+++ herbstluftwm/cycle.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-if [ "$(herbstclient get_attr tags.focus.tiling.focused_frame.client_count)" -gt 2 ] \
-    || [ "$(herbstclient get_attr tags.focus.tiling.focused_frame.algorithm)" = "vertical" ]; then
-    herbstclient cycle_layout 1 max vertical
-else
-    herbstclient cycle
-fi
+[ "$(herbstclient get_attr tags.focus.tiling.root.0.client_count)" -lt 2 ] && return
+[ "$(herbstclient get_attr tags.focus.tiling.focused_frame.index)" -eq 1 ] && herbstclient focus l
+herbstclient cycle "$1"1
