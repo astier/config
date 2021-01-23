a9de8140d307f43739af5718504bae775353553c
aleks <aleks.stier@icloud.com>
2021-01-23 13:16:26 +0100

    [hlwm][fix] cycle.sh wouldn't cycle if maximized
    If only one frame exists then:
    
    ```sh
    herbstclient get_attr tags.focus.tiling.focused_frame.index
    ```
    
    returns an empty string instead of 0. This caused cycle.sh to break.

--- herbstluftwm/cycle.sh
+++ herbstluftwm/cycle.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
 focused_frame=$(herbstclient get_attr tags.focus.tiling.focused_frame.index)
-unfocused_frame=0; [ "$focused_frame" -eq 0 ] && unfocused_frame=1
+if [ -z "$focused_frame" ] && [ "$(herbstclient get_attr tags.focus.tiling.focused_frame.client_count)" -gt 1 ]; then
+    herbstclient cycle "$1"1
+    return
+fi
 
+unfocused_frame=0; [ "$focused_frame" -eq 0 ] && unfocused_frame=1
 if [ "$(herbstclient get_attr tags.focus.tiling.root."$focused_frame".client_count)" -gt 1 ]; then
     herbstclient cycle "$1"1
 elif [ "$(herbstclient get_attr tags.focus.tiling.root."$unfocused_frame".client_count)" -gt 1 ]; then
