edba4a9f6694a663902fcb450f2b498c19af4a6f
aleks <aleks.stier@icloud.com>
2021-02-04 17:21:08 +0100

    [hlwm][cycle] dont use cycle_or_tile

--- herbstluftwm/cycle.sh
+++ herbstluftwm/cycle.sh
@@ -20,14 +20,14 @@ cycle_or_tile() {
 
 focused_frame=$(herbstclient get_attr tags.focus.tiling.focused_frame.index)
 if [ -z "$focused_frame" ] && [ "$(herbstclient get_attr tags.focus.tiling.focused_frame.client_count)" -gt 1 ]; then
-    cycle_or_tile "$focused_frame" "$1"
+    herbstclient cycle "$1"1
     return
 fi
 
 unfocused_frame=0; [ "$focused_frame" -eq 0 ] && unfocused_frame=1
 if [ "$(herbstclient get_attr tags.focus.tiling.root."$focused_frame".client_count)" -gt 1 ]; then
-    cycle_or_tile "$focused_frame" "$1"
+    herbstclient cycle "$1"1
 elif [ "$(herbstclient get_attr tags.focus.tiling.root."$unfocused_frame".client_count)" -gt 1 ]; then
     herbstclient cycle_frame
-    cycle_or_tile "$unfocused_frame" "$1"
+    herbstclient cycle "$1"1
 fi
