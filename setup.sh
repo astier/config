f74911ddbab5368498673ea236ab7d1f8c0eb3ba
Aleksandrs Stier <aleks.stier@icloud.com>
2022-11-16 02:00:17 +0100

    Remove newsboat

--- setup.sh
+++ setup.sh
@@ -76,7 +76,6 @@ if groups "$USER" | grep -q wheel; then
     copy systemd/resolved.conf.d /etc/systemd
     copy systemd/system.conf.d /etc/systemd
     link herbstluftwm "$XDG_CONFIG_HOME"
-    link newsboat "$XDG_CONFIG_HOME"
     link pacman "$XDG_CONFIG_HOME"
     link sx "$XDG_CONFIG_HOME"
     link sxhkd "$XDG_CONFIG_HOME"
