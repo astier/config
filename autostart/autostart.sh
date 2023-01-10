d49b1badf02758a8007e77265818cf095b4bc087
Aleksandrs Stier <aleks.stier@icloud.com>
2023-01-10 12:09:37 +0500

    [bstatus] use new parameters

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 alttab -bg '#2e3440' -fg "#2e3440" -frame "#434c5e" -d 1 -bw 1 -t 64x64 -i 64x64 &
-bstatus -l &
+bstatus &
 hsetroot -cover "$XDG_CONFIG_HOME/wallpaper"
 monitor
 sxhkd &
