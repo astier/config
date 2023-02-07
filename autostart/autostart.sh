b776153d45b207539834a8749a35b974830afbdf
Aleksandrs Stier <aleks.stier@icloud.com>
2023-02-08 00:55:32 +0500

    [hlwm] replace sswm

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -2,14 +2,13 @@
 
 alttab -bg '#2e3440' -fg "#2e3440" -frame "#434c5e" -d 1 -bw 1 -t 64x64 -i 64x64 &
 bstatus &
+hsetroot -cover ~/data/pictures/wallpapers/forest_trees_fog.jpg
 monitor
 sxhkd &
+tint2 &
 xbanish &
+xcompmgr &
 xkbcomp "$XDG_CONFIG_HOME/xkeymap" "$DISPLAY"
 
 "$BROWSER" &
-if tmux has-session; then
-    "$TERMINAL" -e tmux attach &
-else
-    "$TERMINAL" -e tmux new \; splitw -h &
-fi
+"$TERMINAL" -e tmux new -A &
