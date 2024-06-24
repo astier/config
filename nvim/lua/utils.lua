b875cc2f66787a618a9481d2782c580cf0e91e1c
Aleksandrs Stier <aleks.stier@icloud.com>
2024-06-24 12:23:44 +0200

    nvim:utils:get_visual_selection: simplify

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,26 +1,15 @@
 local fn = vim.fn
 local getpos = fn.getpos
-local line2byte = fn.line2byte
 
 local M = {}
 
 function M.get_visual_selection()
-  local line_start, column_start, line_end, column_end
-  if fn.mode() == 'v' then
-    line_start, column_start = unpack(getpos('v'), 2, 3)
-    line_end, column_end = unpack(getpos('.'), 2, 3)
-  else
-    line_start, column_start = unpack(getpos("'<"), 2, 3)
-    line_end, column_end = unpack(getpos("'>"), 2, 3)
+  local col_start = getpos('v')[3]
+  local col_end = getpos('.')[3]
+  if col_start > col_end then
+    col_start, col_end = col_end, col_start
   end
-  if (line2byte(line_start) + column_start) > (line2byte(line_end) + column_end) then
-    line_start, column_start, line_end, column_end = line_end, column_end, line_start, column_start
-  end
-  local lines = fn.getline(line_start, line_end)
-  if #lines == 0 then return '' end
-  lines[#lines] = lines[#lines]:sub(1, column_end)
-  lines[1] = lines[1]:sub(column_start)
-  return table.concat(lines, "\n")
+  return vim.api.nvim_get_current_line():sub(col_start, col_end)
 end
 
 return M
