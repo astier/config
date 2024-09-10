e150408368cf034244b4bf19d4e71f8e5b20a9c8
Aleksandrs Stier <aleks.stier@icloud.com>
2024-09-10 14:30:51 +0200

    mozilla: " -> '

--- mozilla/user.js
+++ mozilla/user.js
@@ -1,33 +1,33 @@
 // Enable userChrome.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true)
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true)
 
 // Disable about:config warning
-user_pref("browser.aboutConfig.showWarning", false);
+user_pref('browser.aboutConfig.showWarning', false);
 
 // Show compact-mode as a density-option
-user_pref("browser.compactmode.show", true);
+user_pref('browser.compactmode.show', true);
 
 // Disabel detachable tabs
-user_pref("browser.tabs.allowTabDetach", false);
+user_pref('browser.tabs.allowTabDetach', false);
 
 // Disable tabmanager in the right corner
-user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref('browser.tabs.tabmanager.enabled', false);
 
 // Disable thumbnails in search-results
-user_pref("browser.urlbar.richSuggestions.featureGate", false);
+user_pref('browser.urlbar.richSuggestions.featureGate', false);
 
 // Disable pocket-extension
-user_pref("extensions.pocket.enabled", false);
+user_pref('extensions.pocket.enabled', false);
 
 // Disable reader-mode
-user_pref("reader.parse-on-load.enabled", false);
+user_pref('reader.parse-on-load.enabled', false);
 
 // Prevent right mouse-button from instantly clicking first option
-user_pref("ui.context_menus.after_mouseup", true);
+user_pref('ui.context_menus.after_mouseup', true);
 
 // Disable showing menubar after pressing alt-button
-user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref('ui.key.menuAccessKeyFocuses', false);
 
 // Enable hardware-acceleration (check arch-wiki)
-user_pref("gfx.webrender.all", true);
-user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref('gfx.webrender.all', true);
+user_pref('media.ffmpeg.vaapi.enabled', true);
